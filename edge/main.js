d273da64f9b65ebe6995ae2f6392171d6251049f
wapenshaw <thescarletcars@gmail.com>
2025-05-16 22:59:27 +0530

    Refactor code style and update README for consistency across Chromium and Edge versions

--- edge/main.js
+++ edge/main.js
@@ -1,4 +1,4 @@
-const NRMTstyles = document.createElement("style");
+const NRMTstyles = document.createElement('style');
 NRMTstyles.innerHTML = `
 .NRMTnode,
 #NRMTtooltip {
@@ -21,168 +21,167 @@ NRMTstyles.innerHTML = `
     background-color: rgba(0, 0, 0, 0.8);
     color: white;
 }
-`
+`;
 document.head.appendChild(NRMTstyles);
 
 const userOptions = {};
 
-chrome.storage.sync.get("index", function(item) {
-    if (item && item.index) {
-        item.index.forEach(node => {
-            userOptions[node.subreddit] = {
-                weight: parseFloat(node.weight),
-                color: node.color
-            };
-        });
-    }
+chrome.storage.sync.get('index', function (item) {
+	if (item && item.index) {
+		item.index.forEach((node) => {
+			userOptions[node.subreddit] = {
+				weight: parseFloat(node.weight),
+				color: node.color,
+			};
+		});
+	}
 });
 
-const userToNRMTnode = {}
-,     userToTooltipInfo = {};
+const userToNRMTnode = {},
+	userToTooltipInfo = {};
 
-const NRMT_TOOLTIP_LEFT_OFFSET = 65
-,     NRMT_TOOLTIP_TOP_OFFSET = 20;
+const NRMT_TOOLTIP_LEFT_OFFSET = 65,
+	NRMT_TOOLTIP_TOP_OFFSET = 20;
 
-const tooltip = document.createElement("div");
-tooltip.id = "NRMTtooltip";
-const tooltipNameHeader = document.createElement("h3");
+const tooltip = document.createElement('div');
+tooltip.id = 'NRMTtooltip';
+const tooltipNameHeader = document.createElement('h3');
 tooltip.appendChild(tooltipNameHeader);
-tooltip.appendChild(document.createElement("hr"));
-const tooltipContributionsContainer = document.createElement("div");
+tooltip.appendChild(document.createElement('hr'));
+const tooltipContributionsContainer = document.createElement('div');
 tooltip.appendChild(tooltipContributionsContainer);
-const siteTable = document.getElementById("siteTable");
+const siteTable = document.getElementById('siteTable');
 siteTable.insertBefore(tooltip, siteTable.firstChild);
 
 function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 function makeToolTipInfoItem(sub, score) {
-    const subLower = sub.toLowerCase();
-    let postfix, c;
-    if (subLower in userOptions) {
-        postfix = "*";
-        c = userOptions[subLower]["color"];
-    } else {
-        postfix = "";
-        c = "white";
-    }
-    return {
-        text: `r/${sub}: ${score}${postfix}`,
-        color: c
-    };
+	const subLower = sub.toLowerCase();
+	let postfix, c;
+	if (subLower in userOptions) {
+		postfix = '*';
+		c = userOptions[subLower]['color'];
+	} else {
+		postfix = '';
+		c = 'white';
+	}
+	return {
+		text: `r/${sub}: ${score}${postfix}`,
+		color: c,
+	};
 }
 
 function makeNRMTnode(parsed) {
-    const username = parsed["children"][0]["data"]["author"]
-    ,     frequented = {};
-    
-    for (let i = 0; i < parsed["dist"]; i++) {
-        const sub = parsed["children"][i]["data"]["subreddit"];
-        if (sub in frequented) {
-            frequented[sub]++;
-        } else {
-            frequented[sub] = 1;
-        }
-    }
+	const username = parsed['children'][0]['data']['author'],
+		frequented = {};
 
-    for (let sub in frequented) {
-        const lower = sub.toLowerCase();
-        if (lower in userOptions) {
-            frequented[sub] *= userOptions[lower]["weight"];
-            if (frequented[sub] == 0) {
-                delete frequented[sub];
-            }
-        }
-    }
+	for (let i = 0; i < parsed['dist']; i++) {
+		const sub = parsed['children'][i]['data']['subreddit'];
+		if (sub in frequented) {
+			frequented[sub]++;
+		} else {
+			frequented[sub] = 1;
+		}
+	}
 
-    const sortable = [];
-    for (key in frequented) {
-        sortable.push([key, frequented[key]]);
-    }
-    sortable.sort(function(a, b) {
-        return a[1] - b[1];
-    });
-    
-    const mostFrequented = sortable[sortable.length - 1][0];
+	for (let sub in frequented) {
+		const lower = sub.toLowerCase();
+		if (lower in userOptions) {
+			frequented[sub] *= userOptions[lower]['weight'];
+			if (frequented[sub] == 0) {
+				delete frequented[sub];
+			}
+		}
+	}
 
-    const textNode = document.createElement("a");
-    textNode.appendChild(document.createTextNode(mostFrequented));
-    textNode.setAttribute("href", `https://old.reddit.com/r/${mostFrequented}`);
-    textNode.setAttribute("target", "_blank");
-    textNode.style.color = "black";
+	const sortable = [];
+	for (key in frequented) {
+		sortable.push([key, frequented[key]]);
+	}
+	sortable.sort(function (a, b) {
+		return a[1] - b[1];
+	});
 
-    const NRMTnode = document.createElement("span");
-    NRMTnode.appendChild(textNode);
-    NRMTnode.className = "NRMTnode";
+	const mostFrequented = sortable[sortable.length - 1][0];
 
-    userToNRMTnode[username] = NRMTnode;
+	const textNode = document.createElement('a');
+	textNode.appendChild(document.createTextNode(mostFrequented));
+	textNode.setAttribute('href', `https://old.reddit.com/r/${mostFrequented}`);
+	textNode.setAttribute('target', '_blank');
+	textNode.style.color = 'black';
 
-    userToTooltipInfo[username] = [];
-    let i = sortable.length;
-    while (--i >= 0 && userToTooltipInfo[username].length < 10) {
-        userToTooltipInfo[username].push(makeToolTipInfoItem(sortable[i][0], sortable[i][1]));
-    }
-    if (i == 0) {
-        userToTooltipInfo[username].push(makeToolTipInfoItem(sortable[0][0], sortable[0][1]));
-    }
-    else if (i > 0) {
-        userToTooltipInfo[username].push({
-            text: `... [${i + 1} more subs]`,
-            color: "white"
-        });
-    }
+	const NRMTnode = document.createElement('span');
+	NRMTnode.appendChild(textNode);
+	NRMTnode.className = 'NRMTnode';
+
+	userToNRMTnode[username] = NRMTnode;
+
+	userToTooltipInfo[username] = [];
+	let i = sortable.length;
+	while (--i >= 0 && userToTooltipInfo[username].length < 10) {
+		userToTooltipInfo[username].push(makeToolTipInfoItem(sortable[i][0], sortable[i][1]));
+	}
+	if (i == 0) {
+		userToTooltipInfo[username].push(makeToolTipInfoItem(sortable[0][0], sortable[0][1]));
+	} else if (i > 0) {
+		userToTooltipInfo[username].push({
+			text: `... [${i + 1} more subs]`,
+			color: 'white',
+		});
+	}
 }
 
 function NRMTnode_in_tagline(tagline) {
-    return tagline.getElementsByClassName("NRMTnode").length > 0;
+	return tagline.getElementsByClassName('NRMTnode').length > 0;
 }
 
 function mainLoop() {
-    const taglines = document.getElementsByClassName("tagline");
-    for (let i = 0; i < taglines.length; i++) {
-        if (!NRMTnode_in_tagline(taglines[i])) {
-            const authorTag = taglines[i].getElementsByClassName("author")[0];
-            if (authorTag != null) {
-                const username = authorTag.innerHTML;
-                if (username in userToNRMTnode) {
-                    if (userToNRMTnode[username] != null) {
-                        const n = userToNRMTnode[username].cloneNode(true);
-                        n.onmouseenter = function() {
-                            tooltip.style.visibility = "visible";
-                            tooltip.style.left = n.offsetLeft + NRMT_TOOLTIP_LEFT_OFFSET + "px";
-                            tooltip.style.top = n.offsetTop + NRMT_TOOLTIP_TOP_OFFSET + "px";
-                            tooltipNameHeader.innerText = username;
-                            tooltipContributionsContainer.innerHTML = "";
-                            userToTooltipInfo[username].forEach(infoNode => {
-                                const p = document.createElement("p");
-                                p.appendChild(document.createTextNode(infoNode.text));
-                                p.style.color = infoNode.color;
-                                tooltipContributionsContainer.appendChild(p);
-                            });
-                        }
-                        n.onmouseleave = function() {
-                            tooltip.style.visibility = "hidden";
-                        }
-                        insertAfter(n, authorTag);
-                    }
-                } else {
-                    userToNRMTnode[username] = null;
-                    const request = new XMLHttpRequest();
-                    const url = `https://old.reddit.com/user/${username}.json?limit=100`;
-                    request.open("GET", url);
-                    request.onreadystatechange = function() {
-                        if (request.readyState == 4 && request.status == 200) {
-                            const parsed = JSON.parse(request.responseText);
-                            makeNRMTnode(parsed["data"]);
-                        }
-                    }
-                    request.send();
-                }   
-            }
-        }
-    }
+	const taglines = document.getElementsByClassName('tagline');
+	for (let i = 0; i < taglines.length; i++) {
+		if (!NRMTnode_in_tagline(taglines[i])) {
+			const authorTag = taglines[i].getElementsByClassName('author')[0];
+			if (authorTag != null) {
+				const username = authorTag.innerHTML;
+				if (username in userToNRMTnode) {
+					if (userToNRMTnode[username] != null) {
+						const n = userToNRMTnode[username].cloneNode(true);
+						n.onmouseenter = function () {
+							tooltip.style.visibility = 'visible';
+							tooltip.style.left = n.offsetLeft + NRMT_TOOLTIP_LEFT_OFFSET + 'px';
+							tooltip.style.top = n.offsetTop + NRMT_TOOLTIP_TOP_OFFSET + 'px';
+							tooltipNameHeader.innerText = username;
+							tooltipContributionsContainer.innerHTML = '';
+							userToTooltipInfo[username].forEach((infoNode) => {
+								const p = document.createElement('p');
+								p.appendChild(document.createTextNode(infoNode.text));
+								p.style.color = infoNode.color;
+								tooltipContributionsContainer.appendChild(p);
+							});
+						};
+						n.onmouseleave = function () {
+							tooltip.style.visibility = 'hidden';
+						};
+						insertAfter(n, authorTag);
+					}
+				} else {
+					userToNRMTnode[username] = null;
+					const request = new XMLHttpRequest();
+					const url = `https://old.reddit.com/user/${username}.json?limit=100`;
+					request.open('GET', url);
+					request.onreadystatechange = function () {
+						if (request.readyState == 4 && request.status == 200) {
+							const parsed = JSON.parse(request.responseText);
+							makeNRMTnode(parsed['data']);
+						}
+					};
+					request.send();
+				}
+			}
+		}
+	}
 }
 
 setInterval(mainLoop, 100);
-console.log("NRMT ready");
+console.log('NRMT ready');
--- edge/popup/popup.js
+++ edge/popup/popup.js
@@ -1,124 +1,128 @@
-const new_btn = document.getElementById("new");
-new_btn.onclick = function() {
-    newCollection();
-}
+const new_btn = document.getElementById('new');
+new_btn.onclick = function () {
+	newCollection();
+};
 let changesMade = false;
 const alreadySeen = new Set();
 
-function newCollection(sub="", weight=1, color="white") {
-    const count = document.getElementsByClassName("collectionWrapper").length;
-    const e = document.createElement("div");
-    e.className = "collectionWrapper";
-    const defaultColor = (!(count & 1)) ? "lightgray" : "white";
-    e.style.backgroundColor = defaultColor;
-    e.onmouseenter = function() {
-        e.style.backgroundColor = "lightgreen";
-    }
-    e.onmouseleave = function() {
-        e.style.backgroundColor = defaultColor;
-    }
+function newCollection(sub = '', weight = 1, color = 'white') {
+	const count = document.getElementsByClassName('collectionWrapper').length;
+	const e = document.createElement('div');
+	e.className = 'collectionWrapper';
+	const defaultColor = !(count & 1) ? 'lightgray' : 'white';
+	e.style.backgroundColor = defaultColor;
+	e.onmouseenter = function () {
+		e.style.backgroundColor = 'lightgreen';
+	};
+	e.onmouseleave = function () {
+		e.style.backgroundColor = defaultColor;
+	};
 
-    e.appendChild(document.createTextNode("/r/"));
-    const subInput = document.createElement("input");
-    subInput.value = sub;
-    subInput.onkeydown = function(e) {
-        if (e.key.match("[a-zA-Z0-9_]") || e.key == "Backspace") {
-            changesMade = true;
-            return true;
-        }
-        return false;
-    }
-    e.appendChild(subInput);
-    
-    e.appendChild(document.createTextNode(" | Weight:"))
-    const weightInput = document.createElement("input");
-    weightInput.style.width = "30px";
-    weightInput.value = weight;
-    weightInput.style.color = "green";
-    weightInput.onkeydown = function(e) {
-        if (e.key.match("[0-9\.]") || e.key == "Backspace") {
-            const future = (e.key == "Backspace") ? weightInput.value.substr(0, weightInput.value.length - 1) : weightInput.value + e.key;
-            if (future.length >= 5) { return false; }
-            if (future.match("^([0-9]+|([0-9]+\.[0-9]+))$")) {
-                weightInput.style.color = "green";
-                changesMade = true;
-            } else {
-                weightInput.style.color = "red";
-                changesMade = false;
-            }
-            return true;
-        }
-        return false;
-    } 
-    e.appendChild(weightInput);
-    
-    e.appendChild(document.createTextNode(" | Color:"));
-    const colorPreview = document.createElement("div");
-    const colorSelect = document.createElement("select");
-    ["white", "red", "pink", "cyan", "green", "orange", "yellow"].forEach(x => {
-        const option = document.createElement("option");
-        if (x == color) {
-            option.setAttribute("selected", "");
-        }
-        option.value = x;
-        option.appendChild(document.createTextNode(x));
-        colorSelect.appendChild(option);
-    });
-    colorSelect.onchange = function() {
-        changesMade = true;
-        colorPreview.style.backgroundColor = colorSelect.value;
-    }
-    e.appendChild(colorSelect);
+	e.appendChild(document.createTextNode('/r/'));
+	const subInput = document.createElement('input');
+	subInput.value = sub;
+	subInput.onkeydown = function (e) {
+		if (e.key.match('[a-zA-Z0-9_]') || e.key == 'Backspace') {
+			changesMade = true;
+			return true;
+		}
+		return false;
+	};
+	e.appendChild(subInput);
 
-    colorPreview.className = "colorPreview";
-    colorPreview.style.backgroundColor = color;
-    e.appendChild(colorPreview);
+	e.appendChild(document.createTextNode(' | Weight:'));
+	const weightInput = document.createElement('input');
+	weightInput.style.width = '30px';
+	weightInput.value = weight;
+	weightInput.style.color = 'green';
+	weightInput.onkeydown = function (e) {
+		if (e.key.match('[0-9.]') || e.key == 'Backspace') {
+			changesMade = true; // Ensure changesMade is true if a relevant key is pressed
+			const future = e.key == 'Backspace' ? weightInput.value.substr(0, weightInput.value.length - 1) : weightInput.value + e.key;
+			if (future.length >= 5) {
+				return false;
+			}
+			if (future.match('^([0-9]+|([0-9]+.[0-9]+))$')) {
+				weightInput.style.color = 'green';
+			} else {
+				weightInput.style.color = 'red';
+				// Removed 'changesMade = false;' to prevent unsaved data
+			}
+			return true;
+		}
+		return false;
+	};
+	e.appendChild(weightInput);
 
-    e.appendChild(document.createTextNode(" | "));
-    const eraseButton = document.createElement("button");
-    eraseButton.innerHTML = "Erase";
-    eraseButton.onclick = function() {
-        e.remove();
-        changesMade = true;
-    }
-    e.appendChild(eraseButton);
+	e.appendChild(document.createTextNode(' | Color:'));
+	const colorPreview = document.createElement('div');
+	const colorSelect = document.createElement('select');
+	['white', 'red', 'pink', 'cyan', 'green', 'orange', 'yellow'].forEach((x) => {
+		const option = document.createElement('option');
+		if (x == color) {
+			option.setAttribute('selected', '');
+		}
+		option.value = x;
+		option.appendChild(document.createTextNode(x));
+		colorSelect.appendChild(option);
+	});
+	colorSelect.onchange = function () {
+		changesMade = true;
+		colorPreview.style.backgroundColor = colorSelect.value;
+	};
+	e.appendChild(colorSelect);
 
-    document.body.appendChild(e);
+	colorPreview.className = 'colorPreview';
+	colorPreview.style.backgroundColor = color;
+	e.appendChild(colorPreview);
+
+	e.appendChild(document.createTextNode(' | '));
+	const eraseButton = document.createElement('button');
+	eraseButton.innerHTML = 'Erase';
+	eraseButton.onclick = function () {
+		e.remove();
+		changesMade = true;
+	};
+	e.appendChild(eraseButton);
+
+	document.body.appendChild(e);
 }
 
 function onLoad(item) {
-    item.index.forEach(node => {
-        newCollection(node.subreddit, node.weight, node.color);
-    });
+	if (item && item.index && Array.isArray(item.index)) {
+		item.index.forEach((node) => {
+			newCollection(node.subreddit, node.weight, node.color);
+		});
+	} else {
+		console.log("NRMT: No valid 'index' data found in storage or storage is empty.", item);
+	}
 }
 
 function saveAndSync() {
-    const elements = document.getElementsByClassName("collectionWrapper");
-    const index = [];
-    alreadySeen.clear();
+	const elements = document.getElementsByClassName('collectionWrapper');
+	const index = [];
+	alreadySeen.clear();
 
-    for (let i = 0; i < elements.length; i++) {
-        const inputs = elements[i].getElementsByTagName("input");
-        const sub = inputs[0].value.toLowerCase();
-        const w = inputs[1].value;
-        const c = elements[i].getElementsByTagName("select")[0].value;
+	for (let i = 0; i < elements.length; i++) {
+		const inputs = elements[i].getElementsByTagName('input');
+		const sub = inputs[0].value.toLowerCase();
+		const w = inputs[1].value;
+		const c = elements[i].getElementsByTagName('select')[0].value;
 
-        if (sub.length > 0 && !alreadySeen.has(sub)) {
-            index.push({subreddit: sub, weight: w, color: c});
-            alreadySeen.add(sub);
-        }
-    }
+		if (sub.length > 0 && !alreadySeen.has(sub)) {
+			index.push({ subreddit: sub, weight: w, color: c });
+			alreadySeen.add(sub);
+		}
+	}
 
-    browser.storage.sync.set({index})
-        .then(null, alert);
+	browser.storage.sync.set({ index }).then(null, console.error); // Changed alert to console.error
 }
 
-browser.storage.sync.get("index")
-    .then(onLoad, alert);
+browser.storage.sync.get('index').then(onLoad, console.error); // Changed alert to console.error
 
-setInterval(function() {
-    if (changesMade) {
-        changesMade = false;
-        saveAndSync();
-    }
+setInterval(function () {
+	if (changesMade) {
+		changesMade = false;
+		saveAndSync();
+	}
 }, 150);
